a7bcbd5790d0a5dc184788f9020ec579e6edd0f7
Pedro Rios <PRios731@gmail.com>
2025-05-12 17:01:34 -0300

    Configure authentication with JWT and security with Spring Security.

---
+++ src/main/java/com/example/finance_control/exceptions/CreatingTokenException.java
@@ -0,0 +1,8 @@
+package com.example.finance_control.exceptions;
+
+public class CreatingTokenException extends RuntimeException{
+
+    public CreatingTokenException(String msg){
+        super(msg);
+    }
+}
